2a3add00beac56659448c460fd23441f1a4f27de
Suthep Sangvirotjanaphat <suthep@gfbd.co.th>
2013-08-07 14:25:11 +0700

    change to-do to resources

---
+++ git-newhand.Web/Controllers/ResourcesController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace git_newhand.Web.Controllers {
+  public class ResourcesController : Controller {
+   
+
+    public ActionResult Index() {
+      return View();
+    }
+
+  }
+}

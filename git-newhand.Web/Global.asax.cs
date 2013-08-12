de4d946de8e92f62c0098ec546c1f5ff76870f6f
Suthep Sangvirotjanaphat <suthep@gfbd.co.th>
2013-08-12 22:31:10 +0700

    Enable Forms Authentication (migrate from Basic to Internet template)

--- git-newhand.Web/Global.asax.cs
+++ git-newhand.Web/Global.asax.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace git_newhand.Web {
@@ -16,6 +17,8 @@ namespace git_newhand.Web {
       WebApiConfig.Register(GlobalConfiguration.Configuration);
       FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
       RouteConfig.RegisterRoutes(RouteTable.Routes);
+      BundleConfig.RegisterBundles(BundleTable.Bundles);
+      AuthConfig.RegisterAuth();
     }
   }
 }
--- git-newhand.Web/obj/Release/Package/PackageTmp/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="git_newhand.Web.MvcApplication" Language="C#" %>
